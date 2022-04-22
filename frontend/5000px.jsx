9c360dd0ad79ccf10f8931e9b5fa0283706ff5bd
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-04-22 15:28:19 -0400

    add store

--- frontend/5000px.jsx
+++ frontend/5000px.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import configureStore from '../frontend/store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
+
+  // BEGIN TESTING
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // END TESTING
+
   ReactDOM.render(<h1>Hello World!</h1>, root)
 })
