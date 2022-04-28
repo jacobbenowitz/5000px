bd1534f5a58a4e672edce9309fe09c32d68b25e2
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-04-28 10:45:05 -0400

    refactor avatar to fetch profile instead of img

--- frontend/components/profile/profile_avatar.jsx
+++ frontend/components/profile/profile_avatar.jsx
@@ -6,15 +6,15 @@ export default class ProfileAvatar extends React.Component {
   }
 
   // TEST: Removed bc url should be in db now, no need to fetch by id
-  // componentDidMount() {
-  //   if (this.props.profileId) {
-  //     this.props.fetchProfilePhoto(this.props.profileId)
-  //   }
-  // }
+  componentDidMount() {
+    if (this.props.profileId) {
+      this.props.fetchProfile(this.props.profileId)
+    }
+  }
 
   render() {
     debugger
-    // if (this.props.pro)
+    
     return (
       <img src = {this.props.avatar }/>
     )
--- frontend/components/profile/profile_avatar_container.jsx
+++ frontend/components/profile/profile_avatar_container.jsx
@@ -1,16 +1,16 @@
 import { connect } from "react-redux"
-import { fetchProfilePhoto } from "../../actions/photos/profile_photos_actions"
+import { fetchProfile } from "../../actions/profile/profile_actions"
 import ProfileAvatar from "./profile_avatar"
 
 const mapStateToProps = state => {
   const profileId = state.session.profile
+  debugger
   return {
-    profileId: profileId,
     avatar: state.entities.profiles[profileId].profile_avatar
   }
 }
 const mapDispatchToProps = dispatch => ({
-  fetchProfilePhoto: profileId => dispatch(fetchProfilePhoto(profileId))
+  fetchProfile: profileId => dispatch(fetchProfile(profileId))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfileAvatar)
