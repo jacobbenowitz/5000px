109562825edb1e0f0fd6d6dab5d808577bd7cd2c
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-04-29 00:25:52 -0400

    landing page styling

--- frontend/components/landing/editors_choice.jsx
+++ frontend/components/landing/editors_choice.jsx
@@ -18,8 +18,7 @@ export const EditorsChoiceLanding = (
     </div>
 
     <div id="editors-gallery"
-      className="landing-gallery-container"
-    >
+      className="landing-gallery-container">
       <div className="landing-gallery-header">
         <h4>The best of the best.</h4>
         <span>Our editors are always on the lookout for jaw dropping content for you to discover and stay inspired. Check back weekly to see what’s new.</span>
@@ -30,26 +29,113 @@ export const EditorsChoiceLanding = (
         <div className="editors-gallery-row">
 
           <div className="editors-gallery-col">
-            <img className="landing-gal-img" src="https://my5000px-static.s3.amazonaws.com/800px/jazz-performer-plays-the-saxophone.jpg" alt="" />
-            <img className="landing-gal-img" src="https://my5000px-static.s3.amazonaws.com/800px/los-angeles-vibes.jpg" alt="" />
+            <div className="editors-box">
+              <Link to={'#'} >
+                <img className="editors-gal-img"
+                  src="https://my5000px-static.s3.amazonaws.com/800px/silhouette-of-boy-throwing-a-net-into-the-water.jpg" alt="Photo by Username" />
+              </Link>
+              <div className="editors-img-attribution">
+                <Link to={'#'} className="img-attribution-link">
+                  <span>Photo by Username</span>
+                </Link>
+              </div>
+            </div>
+
+            <div className="editors-box">
+              <Link to={'#'} >
+                <img className="editors-gal-img"
+                  src="https://my5000px-static.s3.amazonaws.com/800px/silhouette-of-fitness-woman-running-on-the-beach.jpg" />
+              </Link>
+              <div className="editors-img-attribution">
+                <Link to={'#'} className="img-attribution-link">
+                  <span>Photo by Username</span>
+                </Link>
+              </div>
+            </div>
           </div>
 
           <div className="editors-gallery-col">
-            <img className="landing-gal-img" src="https://my5000px-static.s3.amazonaws.com/800px/lost-in-the-city.jpg" alt="" />
-            <img className="landing-gal-img" src="https://my5000px-static.s3.amazonaws.com/800px/moody-autumn-day-in-the-dolomites-forest.jpg" alt="" />
+
+            <div className="editors-box">
+              <Link to={'#'} >
+                <img className="editors-gal-img"
+                  src="https://my5000px-static.s3.amazonaws.com/800px/lost-in-the-city.jpg" alt="Photo by Username" />
+              </Link>
+              <div className="editors-img-attribution">
+                <Link to={'#'} className="img-attribution-link">
+                  <span>Photo by Username</span>
+                </Link>
+              </div>
+            </div>
+
+            <div className="editors-box">
+              <Link to={'#'} >
+                <img className="editors-gal-img"
+                  src="https://my5000px-static.s3.amazonaws.com/800px/red-fox-cub-vulpes-vulpes.jpg" alt="Photo by Username" />
+              </Link>
+              <div className="editors-img-attribution">
+                <Link to={'#'} className="img-attribution-link">
+                  <span>Photo by Username</span>
+                </Link>
+              </div>
+            </div>
           </div>
+
           <div className="editors-gallery-col">
-            <img className="landing-gal-img" src="https://my5000px-static.s3.amazonaws.com/800px/red-fox-cub-vulpes-vulpes.jpg" alt="" />
-            <img className="landing-gal-img" src="https://my5000px-static.s3.amazonaws.com/800px/silhouette-of-boy-throwing-a-net-into-the-water.jpg" alt="" />
+            <div className="editors-box">
+              <Link to={'#'} >
+                <img className="editors-gal-img"
+                  src="https://my5000px-static.s3.amazonaws.com/800px/los-angeles-vibes.jpg" alt="Photo by Username" />
+              </Link>
+              <div className="editors-img-attribution">
+                <Link to={'#'} className="img-attribution-link">
+                  <span>Photo by Username</span>
+                </Link>
+              </div>
+            </div>
+
+            <div className="editors-box">
+              <Link to={'#'} >
+                <img className="editors-gal-img"
+                  src="https://my5000px-static.s3.amazonaws.com/800px/moody-autumn-day-in-the-dolomites-forest.jpg" alt="Photo by Username" />
+              </Link>
+              <div className="editors-img-attribution">
+                <Link to={'#'} className="img-attribution-link">
+                  <span>Photo by Username</span>
+                </Link>
+              </div>
+            </div>
+            
           </div>
+
           <div className="editors-gallery-col">
-            <img className="landing-gal-img" src="https://my5000px-static.s3.amazonaws.com/800px/silhouette-of-fitness-woman-running-on-the-beach.jpg" alt="" />
-            <img className="landing-gal-img" src="https://my5000px-static.s3.amazonaws.com/800px/speed-boat.jpg" alt="" />
+            <div className="editors-box">
+              <Link to={'#'} >
+                <img className="editors-gal-img"
+                  src="https://my5000px-static.s3.amazonaws.com/800px/jazz-performer-plays-the-saxophone.jpg" alt="" />
+              </Link>
+              <div className="editors-img-attribution">
+                <Link to={'#'} className="img-attribution-link">
+                  <span>Photo by Username</span>
+                </Link>
+              </div>
+            </div>
+            
+            <div className="editors-box">
+              <Link to={'#'} >
+                <img className="editors-gal-img"
+                  src="https://my5000px-static.s3.amazonaws.com/800px/moody-autumn-day-in-the-dolomites-forest.jpg" alt="Photo by Username" />
+              </Link>
+              <div className="editors-img-attribution">
+                <Link to={'#'} className="img-attribution-link">
+                  <span>Photo by Username</span>
+                </Link>
+              </div>
+            </div>
           </div>
 
         </div>
       </div>
-      
     </div>
 
 
