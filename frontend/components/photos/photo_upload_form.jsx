e435fa9092acc568394a17fbfaab559fc97a4f00
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-04-29 15:18:00 -0400

    add drag and drop to photoUpload ;

--- frontend/components/photos/photo_upload_form.jsx
+++ frontend/components/photos/photo_upload_form.jsx
@@ -12,11 +12,12 @@ export default class PhotoUploadForm extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleFile = this.handleFile.bind(this);
     this.deletePhoto = this.deletePhoto.bind(this);
+    this.dragOverHandler = this.dragOverHandler.bind(this);
+    this.handleDrop = this.handleDrop.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    debugger
 
     const formData = new FormData();
     
@@ -25,16 +26,14 @@ export default class PhotoUploadForm extends React.Component {
       formData.append('photo[title]', this.state.title);
       formData.append('photo[description]', this.state.description);
       formData.append('photo[photo]', this.state.photoFile);
-      debugger
     }
     this.props.uploadPhoto(formData);
   }
 
   handleFile(e) {
+    e.preventDefault();
     const file = e.target.files[0];
     const fileReader = new FileReader();
-
-    // debugger
 
     fileReader.onloadend = () => {
       this.setState({
@@ -67,14 +66,14 @@ export default class PhotoUploadForm extends React.Component {
 
   showOverlay() {
     const overlay = document.querySelector('#img-preview-overlay');
-    overlay.classList.remove('img-overlay-hidden')
-    overlay.classList.add('img-overlay-show')
+    overlay.classList.remove('img-overlay-hidden');
+    overlay.classList.add('img-overlay-show');
   }
   
   hideOverlay() {
     const overlay = document.querySelector('#img-preview-overlay');
-    overlay.classList.remove('img-overlay-show')
-    overlay.classList.add('img-overlay-hidden')
+    overlay.classList.remove('img-overlay-show');
+    overlay.classList.add('img-overlay-hidden');
   }
 
   deletePhoto() {
@@ -84,8 +83,42 @@ export default class PhotoUploadForm extends React.Component {
       photoFile: null,
       photoUrl: null
     }, () => this.toggleDetailForm())
-  }
+  };
 
+  dragOverHandler(e) {
+    e.preventDefault();
+    const dropZone = document.querySelector('#main-drop-zone');
+    dropZone.classList.add('drag-on')
+  }
+  
+  dragExitHandler(e) {
+    e.preventDefault();
+    const dropZone = document.querySelector('#main-drop-zone');
+    dropZone.classList.remove('drag-on')
+  }
+  
+  handleDrop(e) {
+    e.preventDefault();
+    const dropZone = document.querySelector('#main-drop-zone');
+    dropZone.classList.remove('drag-on')
+
+    const file = e.dataTransfer.files['0'];
+    const fileReader = new FileReader();
+
+    fileReader.onloadend = () => {
+      this.setState({
+        photoFile: file,
+        photoUrl: fileReader.result
+      }, () => {
+        this.toggleDetailForm();
+      })
+    };
+
+    if (file) {
+      fileReader.readAsDataURL(file);
+    }
+    document
+  }
 
   render() {
     
@@ -99,67 +132,78 @@ export default class PhotoUploadForm extends React.Component {
         className="fa-solid fa-trash fa-xl"></i> </div>
     
     return (
-      <div className="photo-upload-main">
+      <div className="span-12"
+      >
+        <div id="main-drop-zone"
+          onDrop={this.handleDrop}
+          onDragOver={this.dragOverHandler}
+          onDragExit={this.dragExitHandler}
+        >
+          
+        </div>
         <div className="page-top-banner">
-          <span>Upload</span>
-        </div>
-
-        <div id="photo-upload-step-1">
-          <div id="upload-title-box" >
-            <h4>Upload photos</h4>
-              <div className="upload-input">
-                <input type="file" name="file-upload"
-                  className="file-upload-input"
-                  onChange={this.handleFile} />
-              </div>
-            <span>Or drag and drop photos anywhere on this page</span>
-            <div className="photo-requirements">
-              <strong>Photo requirements</strong>
-              <span>.png, .jpg, .jpeg only</span>
-              <span>Maximum file size is 200MP/megapixels</span>
-              <span>No NSFW content</span>
-            </div>
+            <span>Upload</span>
           </div>
-        </div>
+        <div className="photo-upload-main">
 
-        <div id="image-upload-step-2" className="hidden">
-
-          <div id="image-preview-container"
-            onMouseOver={this.showOverlay}
-            onMouseLeave={this.hideOverlay}
-          >
-            <div className="image-preview">
-              { overlay }
-              { preview }
-              <span className="photo-label-upload">{ this.state.photoFile ? this.state.photoFile.name : "" }</span>
+          <div id="photo-upload-step-1">
+            <div id="upload-title-box" >
+              <h4>Upload photos</h4>
+                <div className="upload-input">
+                  <input type="file" name="file-upload"
+                    className="file-upload-input"
+                    onChange={this.handleFile} />
+                </div>
+              <span>Or drag and drop photos anywhere on this page</span>
+              <div className="photo-requirements">
+                <strong>Photo requirements</strong>
+                <span>.png, .jpg, .jpeg only</span>
+                <span>Maximum file size is 200MP/megapixels</span>
+                <span>No NSFW content</span>
+              </div>
             </div>
           </div>
 
-          <div id="upload-form-container">
-            <form className="photo-upload-form" onSubmit={this.handleSubmit}>
+          <div id="image-upload-step-2" className="hidden">
 
-              <div className="form-input">
-                <label htmlFor="photo-title">Title</label>
-                <input type="text" name="photo-title"
-                  value={ this.state.photoFile ? this.state.photoFile.name : ""}
-                  className="text-input" onChange={this.handleInput('title')}/>
+            <div id="image-preview-container"
+              onMouseOver={this.showOverlay}
+              onMouseLeave={this.hideOverlay}
+            >
+              <div className="image-preview">
+                { overlay }
+                { preview }
+                <span className="photo-label-upload">{ this.state.photoFile ? this.state.photoFile.name : "" }</span>
               </div>
+            </div>
 
-              <div className="form-input">
-                <label htmlFor="photo-description">Description</label>
-                <input type="text" name="photo-description"
-                  className="text-input" onChange={this.handleInput('description')} />
-              </div>
+            <div id="upload-form-container">
+              <form className="photo-upload-form" onSubmit={this.handleSubmit}>
 
-              <button className="upload-photo-button"
-                type="submit">Upload photo</button>
-              
-            </form>
+                <div className="form-input">
+                  <label htmlFor="photo-title">Title</label>
+                  <input type="text" name="photo-title"
+                    value={ this.state.photoFile ? this.state.photoFile.name : ""}
+                    className="text-input" onChange={this.handleInput('title')}/>
+                </div>
+
+                <div className="form-input">
+                  <label htmlFor="photo-description">Description</label>
+                  <input type="text" name="photo-description"
+                    className="text-input" onChange={this.handleInput('description')} />
+                </div>
+
+                <button className="upload-photo-button"
+                  type="submit">Upload photo</button>
+                
+              </form>
+            </div>
+
           </div>
 
+
         </div>
-
-
+    
       </div>
     )
   }
