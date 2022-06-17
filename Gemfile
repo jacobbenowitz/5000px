d0a2ab99d0c9dd59feae5d009506df70215e4e47
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-06-16 23:11:12 -0400

    add main to package.json, remove yarn.lock, remove commented out code from webpack.config

--- Gemfile
+++ Gemfile
@@ -17,9 +17,8 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'mini_racer', platforms: :ruby
 
 gem 'jquery-rails'
-
 # Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.2'
+# gem 'coffee-rails', '~> 4.2'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   context: __dirname,
   entry: './frontend/5000px.jsx',
-  // entry: ['babel-polyfill', './frontend/5000px.jsx'],
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
@@ -29,9 +28,5 @@ module.exports = {
   devtool: 'source-map',
   resolve: {
     extensions: [".js", ".jsx", "*"],
-    // fallback: {
-    //   "path": require.resolve('path-browserify'),
-    //   "url": false
-    // },
   }
 };
